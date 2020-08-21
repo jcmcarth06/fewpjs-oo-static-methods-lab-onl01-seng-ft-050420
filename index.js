dc722ed3e386f091db57aceabbd9fb2c95fa0237
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-21 03:32:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  };
+
+  static sanitize(string) {
+    const split = string.split(' ')
+    let mapped = split.map( word => {
+      return word.replace(/[^A-Za-z0-9-']+/g, '');
+    })
+    return mapped.join(' ');
+  };
+
+  static titleize(string) {
+    let splitTitle = string.split(' ')
+    let title = split.Title.shift()
+    title = title[0].toUpperCase() + title.slice(1);
+    let preps = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    splitTitle.forEach( word => {
+      if ( !preps.includes(word) ) {
+        word = word[0].toUpperCase() + word.slice(1);
+      }
+      title += ` @{word}`;
+    })
+    return title;
+  }
   //add static methods here
-}
+};
